4b6b0df22a378c845af519826125413f52c79dfd
julielhote <julie.lhote@firstroot.co>
2021-01-15 16:23:37 +0100

    added User Authentification

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -1,15 +1,18 @@
 import graphene
+import graphql_jwt
 
 import hackernews.links.schema
 import hackernews.users.schema
 
 
-class Query(hackernews.links.schema.Query, graphene.ObjectType):
+class Query(hackernews.users.schema.Query, hackernews.links.schema.Query, graphene.ObjectType):
     pass
 
 
 class Mutation(hackernews.users.schema.Mutation, hackernews.links.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- hackernews/settings.py
+++ hackernews/settings.py
@@ -120,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
+}
+
+AUTHENTIFICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
